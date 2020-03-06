925678c1d0d347d6842b79b6e9dd976b8db127a0
Igorth <igordiasth@gmail.com>
2020-03-06 16:33:27 +1300

    state, show the list of repositories

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import api from '../../services/api';
 
 export default class Main extends Component {
+    state = {
+        repositories: [],
+    }
+
     componentDidMount() {
         this.loadRepositories();
     }
@@ -9,10 +13,18 @@ export default class Main extends Component {
     loadRepositories = async () => {
         const response = await api.get(`/repos`);
 
+        this.setState({ repositories: response.data });
+
         console.log(response.data);
     };
 
     render(){
-        return <h1>teste</h1>
+    return (
+        <div className="repo-list">
+            {this.state.repositories.map(repo => (
+                <h2 key={repo.id}>{repo.name}</h2>
+            ))}
+        </div>
+    )
     }
 }
