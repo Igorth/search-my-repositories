63977d384cc09de04db134449ec452bbcf46d6fb
Igorth <igordiasth@gmail.com>
2020-03-14 12:26:26 +1300

    readme, license, contributing, editor config

---
+++ README.md
@@ -0,0 +1,17 @@
+# Search Git Repositories
+
+Using React I can get information of my repository in GitHub
+
+## Contributing
+
+Please read [CONTRIBUTING.md](https://gist.github.com/PurpleBooth/b24679402957c63ec426) for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Authors
+
+* **Igor Dias** - *Initial work* - [Search Git](https://github.com/Igorth/search-my-repositories)
+
+See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- src/pages/repositorie/index.js
+++ src/pages/repositorie/index.js
@@ -1,7 +1,32 @@
 import React, { Component } from 'react';
+import api from '../../services/api';
+import './styles.css';
 
 export default class Repositorie extends Component {
+    state = {
+        repositorie: {},
+    };
+
+    async componentDidMount() {
+        const { id } = this.props.match.params;
+
+        const response = await api.get(`/repos/${id}`);
+
+        this.setState({ repositorie: response.data });
+    }
+
     render() {
-        return <h1>tete</h1>
+        const { repositorie } = this.state;
+
+        return (
+            <div className="repo-info">
+                <h1>{repositorie.name}</h1>
+                <p>{repositorie.description}</p>
+
+                <p>
+                    URL: <a href={repositorie.html_url}>{repositorie.html_url}</a>
+                </p>
+            </div>
+        );
     }
 }
